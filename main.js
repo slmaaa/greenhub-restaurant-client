0561df247b92ad18b62a00db94ed7feca0c1c0e7
slmaaa <maszelong2001@gmail.com>
2022-04-13 16:00:19 +0800

    notifications dedug UI

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ import Scan from "./scan.js";
 const Main = () => {
     //useStates for routing
     const user = useRef(null);
-    const [isSuccess, setIsSuccess] = useState(true);
+    const [isSuccess, setIsSuccess] = useState(false);
 
     const [currentPage, setCurrentPage] = useState("HOME");
     const [isLoading, setIsLoading] = useState(false);
